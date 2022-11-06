bcab005e73b99d1120b216d69ee56e105eba5f29
jchoi10 <noreply>
2022-11-06 15:45:46 -0500

    add jquery directory

---
+++ server.js
@@ -0,0 +1,9 @@
+<reference path="./@types/jquery" />
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require(./routes/apiRoutes)
