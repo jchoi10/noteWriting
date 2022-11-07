db7c1f824e7d625733c41c33ea40fda66103a5fd
jchoi10 <noreply>
2022-11-06 21:47:05 -0500

    add port description

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRouts = require('./routes/htmlRoutes');
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
