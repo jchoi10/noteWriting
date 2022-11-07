abc635ded5feb0848b190932176800d5d32eff8d
jchoi10 <noreply>
2022-11-07 00:21:04 -0500

    add html api route

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes/apiRoutes');
 const htmlRouts = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRouts);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
