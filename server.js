42992b00556dd7d9d1f41e6854cbcfdafc9c6320
jchoi10 <noreply>
2022-11-06 21:45:33 -0500

    add routes

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const apiRoutes = require(./routes/apiRoutes)
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRouts = require('./routes/htmlRoutes');
