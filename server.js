cc9ceeaab21c2c59f338183bca49f561d693adfe
jchoi10 <noreply>
2022-11-07 01:59:09 -0500

    heroku repo connect

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const apiRoutes = require('./routes/apiRoutes/noteRoutes');
-const htmlRouts = require('./routes/htmlRoutes');
-
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRouts);
+
+require('./routes/apiRoutes/noteRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
